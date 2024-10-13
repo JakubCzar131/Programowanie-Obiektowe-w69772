68dbfa906bdb7d2730b870913d8580cfd2ecf88c
Jakub Czarnota <w69772@student.wsiz.edu.pl>
2024-10-13 09:35:16 +0200

    Lab1_1

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("stoj"); // 
+
+int a, b = 34;
+double c = 34,34;
+short s = 1;
+
+long longbA = 121212121212121;
+char znak = 'a';
+string name = "jan kowal";
+bool type = true;
+Console.WriteLine("Poda liczb");
+int number = Convert.ToUInt32(Console.ReadLine());
+Console.WriteLine("Podana liczba:" + number);
+
+
+int x;
+
+string name = Console.ReadLine();
+
+for (int i = 0; i <= x; i++)
+{
+    Console.WriteLine(i);
+
+}
+
+Console.writline
