223c906b32eef583c06b9daadcb86fde868df64d
Jakub Czarnota <w69772@student.wsiz.edu.pl>
2024-10-13 11:13:13 +0200

    LAB1_2

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -1,30 +1,96 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Reflection.Metadata;
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("stoj"); // 
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("stoj"); // 
+//int b = 34;
+//double c = 34;
+//short s = 1;
 
-int a, b = 34;
-double c = 34,34;
-short s = 1;
-
-long longbA = 121212121212121;
-char znak = 'a';
-string name = "jan kowal";
-bool type = true;
-Console.WriteLine("Poda liczb");
-int number = Convert.ToUInt32(Console.ReadLine());
-Console.WriteLine("Podana liczba:" + number);
+//long longbA = 121212121212121;
+//char znak = 'a';
+//string sumaa = "jan kowal";
+//bool type = true;
+//Console.WriteLine("Poda liczb");
+////int number = Convert.ToUInt32(Console.ReadLine());
+////Console.WriteLine("Podana liczba:" + number);
 
 
-int x;
+//int x = 2;
 
-string name = Console.ReadLine();
+////string name = Console.ReadLine();
 
-for (int i = 0; i <= x; i++)
+//for (int i = 0; i <= x; i++)
+//{
+//    Console.WriteLine(i);
+
+//}
+//int a = 1;
+
+//while (a < 5) 
+//{
+//    Console.WriteLine(a);
+//    a++;
+//}
+//do
+//{
+//    Console.WriteLine(a);
+//    a++;
+//}
+//while (a < 5);
+
+//for(int i=0;i<5;i++)
+//{
+//    Console.WriteLine(a);
+//}
+//string[] name = { "jan", "alina" };
+//int[] number = { 1, 2, 3, 3 };
+//Console.WriteLine(number[0]);
+//Console.WriteLine("dlugosc tablicy: " + number.Length);
+zadanie1();
+static void zadanie1()
 {
-    Console.WriteLine(i);
+    // deklaracja zmiennych
+    double a = doubleInput();
+    double b = doubleInput();
+    double c = doubleInput();
+    double delta, x1, x2;
+
+    if (a != 0)
+    {
+        delta = Math.Pow(b, 2) - (4 * a * c);
+        if (delta < 0)
+        {
+            Console.WriteLine("Brak rozwiazania w zbiorze liczb rzeczywistych");
+        }
+        else if (delta == 0)
+        {
+            x1 = -b / (2 * a);
+            Console.WriteLine("jedno rozwiazanie x1 = " + x1);
+        }
+        else
+        {
+            x1 = (-b - Math.Sqrt(delta)) / (2 * a);
+            x2 = (-b + Math.Sqrt(delta)) / (2 * a);
+            Console.WriteLine("Dwa rozwiazania: \t x1 = " + x1 + "\t x2 = " + x2);
+        }
+
+    }
+    else { Console.WriteLine("To nie jest równanie kwadratowe!"); }
+}
+static void zadanie2()
+{
+    Console.WriteLine("K A L K U L A T O R ")
+
 
 }
+static double doubleInput()
+{
+    Console.WriteLine("Podaj liczbe: ");
+    double input = Convert.ToDouble(Console.ReadLine());
+    return input;
+}
 
-Console.writline
+
--- lab1/lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
+++ lab1/lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68dbfa906bdb7d2730b870913d8580cfd2ecf88c")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
