54374d474fa8486155e4e03fb093d1e727676cac
jakubc <jakubc@LAPTOP-O24LAPJ0>
2024-11-07 16:29:43 +0100

    zadanie 3 i 4 lab 1

--- lab1/lab1_zad 3/Program.cs
+++ lab1/lab1_zad 3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-Console.WriteLine("Podaj 10 liczb:");
+using System.Linq;
+Console.WriteLine("zad 3 Podaj 10 liczb:");
 static void Zadanie3()
 {
     double[] lista = new double[10];
@@ -37,9 +38,31 @@ static void Zadanie3()
 
 Zadanie3();
 
+
+Console.WriteLine("zadanie 4 Podaj 10 liczb rzeczywistych: ");
+static void Zadanie4()
+{
+    double[] lista2 = new double[10];
+
+    for (int indeks = 0; indeks < 10; indeks++)
+    {
+        double wartosc = WczytajDouble();
+        lista2[indeks] = wartosc;
+    }
+
+    Console.WriteLine($"Suma: {lista2.Sum()}");
+    Console.WriteLine($"Iloczyn: {lista2.Aggregate(1.0, (a, b) => a * b)}");
+    Console.WriteLine($"Średnia: {lista2.Average()}");
+    Console.WriteLine($"minimum: {lista2.Min()}");
+    Console.WriteLine($"Maximum: {lista2.Max()}");
+}
+
+Zadanie4();
+
+
 static double WczytajDouble()
 {
-    
+
     double wartosc = Convert.ToDouble(Console.ReadLine());
     return wartosc;
 }
